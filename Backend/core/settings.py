00c6d917972f99644b642c4e40e5f8b37e1ebf77
SsantiDev <ssantiago.xz1130@gmail.com>
2026-02-22 14:33:36 -0500

    chore: scaffold initial Django apps structure (users, services, reviews, payments)

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -39,6 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+
+    # Apps
+    'apps.users',
+    'apps.services',
+    'apps.payments',
+    'apps.reviews',
 ]
 
 MIDDLEWARE = [
