e366b69c94c956b71538ff8d1ff88e696b8b2638
SsantiDev <ssantiago.xz1130@gmail.com>
2026-04-12 17:57:01 -0500

    refactor(backend): improve incident viewset access control

--- Backend/apps/support/views.py
+++ Backend/apps/support/views.py
@@ -14,11 +14,16 @@ class IncidentViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         user = self.request.user
         if user.role == 'ADMIN':
-            return Incident.objects.all()
-            
-        # Users can see incidents they reported or related to services they are involved in
-        return Incident.objects.filter(
-            models.Q(reporter=user) |
-            models.Q(service_request__client=user) |
-            models.Q(service_request__worker=user)
-        ).distinct()
+            qs = Incident.objects.all()
+        else:
+            qs = Incident.objects.filter(
+                models.Q(reporter=user) |
+                models.Q(service_request__client=user) |
+                models.Q(service_request__worker=user)
+            ).distinct()
+
+        service_request_id = self.request.query_params.get('service_request')
+        if service_request_id:
+            qs = qs.filter(service_request_id=service_request_id)
+
+        return qs
