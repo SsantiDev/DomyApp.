a7e7f9dffedca75e8e5837bb377137053c015b0e
Ssantidev <ssantiago.xz1130@gmail.com>
2026-05-12 06:38:16 -0500

    chore(users): remove unused signal imports

--- Backend/apps/users/signals.py
+++ Backend/apps/users/signals.py
@@ -1,8 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.conf import settings
 from .models import User, Profile, WorkerProfile, WorkerVerification
-from .services import EmailService
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
