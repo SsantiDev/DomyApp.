e70803bad27a475a543e803270467af0619b53f8
Ssantidev <ssantiago.xz1130@gmail.com>
2026-05-12 06:38:31 -0500

    chore(core): reorder asgi imports

--- Backend/core/asgi.py
+++ Backend/core/asgi.py
@@ -1,14 +1,13 @@
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+from apps.chat.routing import websocket_urlpatterns as chat_ws
+from apps.notifications.routing import websocket_urlpatterns as notif_ws
+from core.middleware import JWTAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 django_asgi_app = get_asgi_application()
-
-from apps.chat.routing import websocket_urlpatterns as chat_ws
-from apps.notifications.routing import websocket_urlpatterns as notif_ws
-from core.middleware import JWTAuthMiddleware
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
